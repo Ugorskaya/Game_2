3fbfc652dbe95e755cd623490ec6ca95d644e281
Ugorskaya <z_942011@mail.ru>
2015-12-23 03:20:24 +0800

    111

--- app/src/main/java/com/example/yana/game_chislo/game1.java
+++ app/src/main/java/com/example/yana/game_chislo/game1.java
@@ -30,53 +30,74 @@ public class game1 extends AppCompatActivity {
 
         ch01 = Integer.parseInt(cos1);
         ch02 = Integer.parseInt(cos2);
-
-        ch1 = ch01;
-        ch2 = ch02 / 2;
         klmn = 0;
-
         vvv = (TextView) findViewById(R.id.vvv);
-        vvv.setText("От " + ch1 + " до" + ch2);
-    }
 
-    public void But(View v) {
+        if (ch01>ch02){ch01=ch02;ch02=Integer.parseInt(cos1);}//правильный порядок
+        if((ch02-ch01)==1){klmn=1;vvv.setText("Это " + ch01 + "?");}//интервал 1
+        else if((ch02-ch01)==0){ch00=ch01;Bb1();}//если интервал 0
+        else{
+        ch1 = ch01;
+        ch2 = ch01+(ch02 -ch01)/2;
+
+        vvv.setText("От " + ch1 + " до " + ch2);
+         }}
+
+
+    public void But(View v) {//интервал больше 1
         if (klmn == 0) {
+            //подсчет интервала
             switch (v.getId()) {
                 case R.id.button1:
                     //если число в интервале
+                    ch01 = ch1;
                     ch02 = ch2;
                     ch2 = ch1 + (ch2 - ch1) / 2;
                     break;
                 case R.id.button2:
                     //число не в интервале
-                    ch01 = ch2;
-                    ch1 = ch2 + 1;
-                    ch2 = ch1 + (ch02 - ch1) / 2;
+                    ch01 = ch2+1;
+                    ch1 = ch01;
+                    ch2 = ((ch02-ch01)/2)+ch01;
                     break;
             }
-            vvv.setText("От " + ch1 + " до" + ch2);
 
-            int ch = ch2 - ch1;
-            if ((ch < 1)) {
-                vvv.setText("Это " + ch1 + "?");
+            //проверка интервала
+            int ch = ch02 - ch01;
+            if ((ch == 1)) {//если равно 1
+                vvv.setText("Это " + ch01 + "?");
                 klmn = 1;
             }
-        } else {
-            switch (v.getId()) {
-                case R.id.button1:
-                    //если число отгадано(ch1)
-                    ch00 = ch01;
-                    break;
-                case R.id.button2:
-                    //число не отгадано(ch2)
-                    ch00 = ch02;
-                    break;
+            else if(ch == 0){//если равно 0
+                ch00=ch01;
+                Bb1();
             }
-            Intent intent1 = new Intent(this, Final.class);
-            intent1.putExtra("ch00", (Integer.toString(ch00)));
-            startActivity(intent1);
-        }
+            else{//если больше 1
+                vvv.setText("От " + ch1 + " до" + ch2);
+            }
 
+        } else {
+            if (klmn == 1) {//если равно 1
+
+                switch (v.getId()) {
+                    case R.id.button1:
+                        //если число отгадано(ch1)
+                        ch00 = ch01;
+                        break;
+                    case R.id.button2:
+                        //число не отгадано(ch2)
+                        ch00 = ch02;
+                        break;
+                }
+                Bb1();
+            }
+        }
+    }
+
+    private void Bb1() {
+        Intent intent1 = new Intent(this, Final.class);
+        intent1.putExtra("ch00", (Integer.toString(ch00)));
+        startActivity(intent1);
     }
 
 
